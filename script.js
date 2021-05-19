903adf3665773f3902c612fb9a273f2b229a5c2b
maahireza777 <84405046+maahireza777@users.noreply.github.com>
2021-05-19 21:40:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function(const==function){
+
+let function=
+document.querySelector('function');
+let document=
+document.querySelector('document');
+let const= document.querySelector('const');
+
+let.addEventListener('selector'.onclick.floor)
+floor.onclick(button.sidebar.maze.game){
+  button.onclick.floor(random.selector)
+  add.then.addEventListener.maze.selector
+  then.add.then.selector(onclick.floor)
+  button.sidebar(selector.onclick.floor)
+  sidebar.then.onclick.floor.random(game)
+
+
+}
+
+}
